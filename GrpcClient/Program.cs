93be19df5c04838b5c05129b5a57f957e28f137e
kishan kasyap anem <kishan3034@gmail.com>
2020-02-28 12:45:05 +0100

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcPrac;
+using System;
+using System.Threading.Channels;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001"); 
+
+            var client = new Greeter.GreeterClient(channel);
+            var user = "you";
+
+            var reply = client.SayHello(new HelloRequest { Name = user });
+            Console.WriteLine("Greeting: " + reply.Message);
+
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
